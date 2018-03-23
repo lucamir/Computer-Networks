4c4ed8b01dd8278a9e96a9cdd357fa2c5046fd76
Luca Mirabella <lucamirabella@MacBook-Pro-di-Luca-2.local>
2018-03-23 23:33:46 +0100

    first commit

---
+++ Esercizio 1 (Connessione UDP)/Esercizio laboratorio 1 UDP/Sender.c
@@ -0,0 +1,32 @@
+/* udp Sender */
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#define RECV_PORT 7550
+#define RECV_IP "192.168.1.11"
+main(){
+	int sockfd;
+	struct sockaddr_in recv_addr;
+	socklen_t lengReciv=sizeof(recv_addr);
+	char line[100];
+	int i,n;
+	time_t mytime=time(NULL);
+	
+	if((sockfd=socket(AF_INET,SOCK_DGRAM,0))<0){
+		printf("\nErrore nell'apertura della socket");
+		return -1;
+	}
+	memset((char *)&recv_addr,0,lengReciv);
+	recv_addr.sin_family=AF_INET;
+	recv_addr.sin_addr.s_addr=inet_addr(RECV_IP);
+	recv_addr.sin_port=htons(RECV_PORT);
+	
+	strftime(line,100,"%d-%m-%Y %H:%M", localtime(&mytime));
+	sendto(sockfd,line,strlen(line)+1,0,(struct sockaddr *)&recv_addr,lengReciv);
+}
---
+++ Esercizio 2 (Connessione UDP Bidirezionale)/serv.c
@@ -0,0 +1,38 @@
+/*** Server UDP ***/
+#include <stdio.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h>
+#define PORT 7550
+main(){ 	
+	int sockfd;
+	struct sockaddr_in servaddr,cliaddr; 
+	char mesg[50],ack[1]={'1'};
+	FILE* fp=fopen("new.txt","w+");
+	socklen_t len=sizeof(cliaddr),servAdd=sizeof(servaddr);
+	sockfd=socket(AF_INET,SOCK_DGRAM,0);
+	memset(&servaddr,0,servAdd); 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr=htonl(INADDR_ANY); 
+	servaddr.sin_port=htons(PORT); 
+	bind(sockfd,(struct sockaddr *)&servaddr,servAdd);
+		
+	printf("Sono in ascolto!\n");
+	
+	while(1){
+		memset(mesg, '\0',sizeof(mesg));
+		recvfrom(sockfd,mesg,50,0,(struct sockaddr *)&cliaddr,&len);
+		sendto(sockfd,ack,1,0,(struct sockaddr *)&cliaddr,len);
+		if(strcmp("./end/.",mesg)==0){
+			fclose(fp);
+			break;
+		}
+		else {
+			fputs(mesg,fp);
+			printf("------------------------------------------------\n"); 
+			printf("Received the following:\n");
+			printf("%s",mesg); 
+			printf("------------------------------------------------\n");
+		}
+	} 
+}
